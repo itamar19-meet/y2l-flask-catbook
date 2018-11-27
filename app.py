3699faf58e9df679c48fa652901b2ad39956e8a3
aboud20-meet <aboud20@meet.mit.edu>
2018-11-27 20:05:25 +0200

    i need to mak a variable

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask  
 from flask import render_template
+from flask import request
 from database import get_all_cats
 
 app = Flask(__name__)
@@ -10,5 +11,31 @@ def catbook_home():
     cats = get_all_cats()
     return render_template("home.html", cats=cats)
 
+@app.route('/cat', methods=['GET', 'POST'])
+def cat():
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+        return render_template('cat.html')
+
+@app.route('/details', methods=['GET', 'POST'])
+def add_cat():
+    if(request.method == 'GET'):
+        return render_template("details.html")
+    else:                                                          
+        n = request.form['name']
+        create_cat(n)
+        print('reived post request')
+        return render_template("home.html")
+@app.route('/', methods=['GET', 'POST'])
+def add_vote():
+
+    if(request.method == 'GET'):
+        return render_template("home.html")
+    else:                             
+        vote_count  = vote_count + 1
+        return render_template("home.html")
+
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug = True)
+
