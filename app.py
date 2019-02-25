a030a59c6096dc134836a4d24629a1bf9149167d
aboud20-meet <aboud20@meet.mit.edu>
2019-02-25 19:39:05 +0200

    finished

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask  
+from flask import Flask,redirect 
 from flask import render_template
 from flask import request
-from database import get_all_cats
+from database import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'YOUR-VERY-SECRET-SHHH'
@@ -23,18 +23,18 @@ def add_cat():
     if(request.method == 'GET'):
         return render_template("details.html")
     else:                                                          
-        n = request.form['name']
-        create_cat(n)
-        print('reived post request')
-        return render_template("home.html")
-@app.route('/', methods=['GET', 'POST'])
+        name = request.form['name']
+        create_cat(name,0)
+        return redirect("/")
+@app.route('/add_vote', methods=['GET', 'POST'])
 def add_vote():
 
     if(request.method == 'GET'):
-        return render_template("home.html")
-    else:                             
-        vote_count  = vote_count + 1
-        return render_template("home.html")
+        return render_template("/home")
+    else:
+        cat_id = request.form['cat_id']                             
+        add_like(cat_id)
+        return redirect("/")
 
 if __name__ == '__main__':
     app.run(debug = True)
--- database.py
+++ database.py
@@ -8,11 +8,15 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def create_cat(name):
-    cat_object = Cat(name=name)
+def create_cat(name,like):
+    cat_object = Cat(name=name,like = like)
     session.add(cat_object)
     session.commit()
 
 def get_all_cats():
     cats = session.query(Cat).all()
     return cats
+def add_like(id):
+	to_like = session.query(Cat).filter_by(id=id).first()
+	to_like.like = to_like.like+1
+	
